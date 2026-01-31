dd5691129145c75d38e7607810e4766f471ee51a
Dave Pearson <davep@davep.org>
2026-01-31 12:07:55 +0000

    :white_check_mark: Add a test for using a pipe within a pipe

--- tests/test_pipe.py
+++ tests/test_pipe.py
@@ -1,6 +1,11 @@
 """Tests for the Pipe class."""
 
 ##############################################################################
+# Python imports.
+from functools import partial
+from operator import add
+
+##############################################################################
 # Local imports.
 from bagofstuff.pipe import Pipe
 
@@ -27,6 +32,14 @@ def test_multi_function_inline() -> None:
 def test_multi_function_via_piple() -> None:
     """We should be able to pass multiple functions inline."""
     assert (Pipe[str, str]() | str.strip | str.upper)("   test   ") == "TEST"
+
+
+##############################################################################
+def test_pipe_with_pipe() -> None:
+    """A pipe should be usable in another pipe's pipeline."""
+    child_pipe = Pipe[int, int](partial(add, 1))
+    main_pipe = Pipe[int, int](child_pipe, child_pipe) | child_pipe | child_pipe
+    assert main_pipe(1) == 5
 
 
 ### test_pipe.py ends here
